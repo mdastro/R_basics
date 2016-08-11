67fd324cb393f788dde74eaa17b71e9fa15a3d34
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2016-08-11 11:13:23 -0300

    Update the_very_basics.R

--- the_very_basics.R
+++ the_very_basics.R
@@ -4,6 +4,9 @@ x <- seq(2,5, length.out=5)
 # Data Frame
 data <- read.csv("/home/mldantas/documentos/dados.csv")
 
+# Data Frame with decimal separation = "," and separation between rows ";"
+data <- read.csv("/home/mldantas/documentos/dados.csv", dec=",", sep=";", header=TRUE)
+
 # Read Header
 head(data)
 
