4ad61990d461b426f47bd77ce0f6ab1a362704af
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2016-08-11 11:10:16 -0300

    Create the_very_basics.R

---
+++ the_very_basics.R
@@ -0,0 +1,11 @@
+x <- seq(2,5, length.out=5)
+# output: [1] 2.00 2.75 3.50 4.25 5.00
+
+# Data Frame
+data <- read.csv("/home/mldantas/documentos/dados.csv")
+
+# Read Header
+head(data)
+
+getwd()
+# output: [1] "/home/mae5755.16"
