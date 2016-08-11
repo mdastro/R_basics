f75631d181f9f76f303ab9b98aa9f73ed7010391
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2016-08-11 11:28:01 -0300

    Update the_very_basics.R

--- the_very_basics.R
+++ the_very_basics.R
@@ -7,6 +7,20 @@ data <- read.csv("/home/mldantas/documentos/dados.csv")
 # Data Frame with decimal separation = "," and separation between rows ";"
 data <- read.csv("/home/mldantas/documentos/dados.csv", dec=",", sep=";", header=TRUE)
 
+# Label example:
+peso <- c(75,60,75,80)
+sexo <- c(1,2,2,1)
+sexo <- factor(sexo, levels = c(1,2), label=c("Masculino", "Feminino")
+dados <- data.frame(peso=peso, sexo=sexo)
+
+# outout:
+#   peso sexo
+# 1   75    1
+# 2   60    2
+# 3   75    2
+# 4   80    1
+
+
 # Read Header
 head(data)
 
